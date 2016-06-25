8d0d121a3acf7133299ac47c158d0a7a0b80126e
Jack T <jt1461@outlook.com>
2016-06-25 16:24:27 -0700

    NodeSchool elementary-electron tutorial 3

--- elementary-electron/index.js
+++ elementary-electron/index.js
@@ -1,1 +1,7 @@
-require('cat-picture')
+var picture = require('cat-picture')
+var src = picture.src
+picture.remove()
+var image = require('lightning-image-poly')
+var viz = new image('#visualization', null, [src], {
+    hullAlgorithm: 'convex'
+})
