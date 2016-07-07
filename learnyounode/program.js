0439c153fc011ac0229029be5d1c7bea74e1cec4
Jack T <jt1461@outlook.com>
2016-07-06 23:10:31 -0700

    learnyounode exercise 2 completed

--- learnyounode/program.js
+++ learnyounode/program.js
@@ -1,1 +1,6 @@
-console.log("HELLO WORLD");
+var sum = 0;
+
+for (i = 2; i < process.argv.length; i++ ) {   // first argument starts at index = 2
+  sum += Number(process.argv[i]);
+}
+console.log(sum);
