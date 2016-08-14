9b8371515e60242e255b8844ca1f5c45c4e1f1ad
JackT <jturn08@outlook.com>
2016-08-13 23:12:53 -0700

    refactor exercise 4 and 5 for readability

--- learnyounode/program4.js
+++ learnyounode/program4.js
@@ -1,13 +1,22 @@
 // My First Async I/O
 
 var fs = require('fs'); // Load fs module from core library
-
 var filepath = process.argv[2]; // Skip first 2 values that include system info
+var totalNewLines = undefined;
 
-fs.readFile(filepath, function calculateNewLines(errId, contents) {
+StartReadFile(StartConsoleLog);
+
+function StartReadFile(callback) {
+  fs.readFile(filepath, function StartProcessFile(errId, contents) {
     if (errId) {
-        return console.error(errId);
+      return console.error(errId);
     }
-    var totalNewLines = contents.toString().split("\n").length - 1; // subtract 1 because last line doesn't end in newline
-    console.log(totalNewLines);
-});
+    totalNewLines = contents.toString().split("\n").length - 1; // subtract 1 because last line doesn't end in newline
+
+    callback();
+  });
+}
+
+function StartConsoleLog() {
+  console.log(totalNewLines);
+}
--- learnyounode/program5.js
+++ learnyounode/program5.js
@@ -5,14 +5,25 @@ var path = require('path'); // Load path Node.js core module
 
 var dirpath = process.argv[2];
 var ext = '.' + process.argv[3];
+var row = undefined;
 
-fs.readdir(dirpath, function processFilenames(errId, contents) {
+StartReadDirectory(CheckExtension);
+
+function StartReadDirectory(callback) {
+  fs.readdir(dirpath, function processFilenames(errId, contents) {
     if (errId) {
-        return console.error(errId);
+      return console.error(errId);
     }
-    contents.forEach( function(row) {
-        if (path.extname(row) == ext) {
-            console.log(row);
-        }
-    })
-});
+    contents.forEach(CheckExtension);
+  });
+}
+
+function CheckExtension(row) {
+  if(path.extname(row) == ext) {
+    StartConsoleLog(row);
+  }
+}
+
+function StartConsoleLog(text) {
+  console.log(text)
+}
