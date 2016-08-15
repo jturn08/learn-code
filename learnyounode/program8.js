e4c05384adb1ffcf46556928f04f40683765765d
JackT <jturn08@outlook.com>
2016-08-14 17:07:10 -0700

    learnyounode exercise 8 code cleanup

--- learnyounode/program8.js
+++ learnyounode/program8.js
@@ -3,20 +3,18 @@ var bl = require('bl');
 
 var RequestUrlPath = process.argv[2];
 
-var req = http.request(RequestUrlPath, function(res){
-  res.pipe(bl(function(err, data) {
-    if (err) {
-      return console.error(err);
-    }
-      console.log(data.toString().length);
-      console.log(data.toString());
-  }))
-}
-
-);
+var req = http.request(RequestUrlPath, function(res) {
+    res.pipe(bl(function(err, data) {
+        if (err) {
+            return console.error(err);
+        }
+        console.log(data.toString().length);
+        console.log(data.toString());
+    }))
+});
 
 req.on('error', function(e) {
-  console.error(e.message);
+    console.error(e.message);
 });
 
 req.end();
