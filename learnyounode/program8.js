18798f6d17b427e1ce6103b4576507d2675f0cc2
JackT <jturn08@outlook.com>
2016-08-14 17:04:28 -0700

    learnyounode exercise 8

---
+++ learnyounode/program8.js
@@ -0,0 +1,22 @@
+const http = require('http');
+var bl = require('bl');
+
+var RequestUrlPath = process.argv[2];
+
+var req = http.request(RequestUrlPath, function(res){
+  res.pipe(bl(function(err, data) {
+    if (err) {
+      return console.error(err);
+    }
+      console.log(data.toString().length);
+      console.log(data.toString());
+  }))
+}
+
+);
+
+req.on('error', function(e) {
+  console.error(e.message);
+});
+
+req.end();
